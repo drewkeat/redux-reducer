5de3397e355272387c4c00cf94d2354504a93659
Drew Keat <thedrewkeat@gmail.com>
2021-11-29 13:50:52 -0700

    commit tree 5c3376f5956b06f98f0d2caf62862e38a4a41083

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      let newFriend = action.friend
+      return {friends: [...state.friends, newFriend]}
+    case "REMOVE_FRIEND":
+      let toRemove = state.friends.findIndex(friend => friend.id === action.id)
+      return {friends: [...state.friends.slice(0, toRemove), ...state.friends.slice(toRemove + 1)]
+      }
+    default:
+      return state
+  }
 }
